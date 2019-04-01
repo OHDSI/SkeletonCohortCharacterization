1a433829308ae7c2493747e5ac2c44a2ecbce55d
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-04-01 19:15:30 +0300

    Performance fixes

--- src/main/java/org/ohdsi/cohortcharacterization/design/FeatureAnalysisWithDistributionImpl.java
+++ src/main/java/org/ohdsi/cohortcharacterization/design/FeatureAnalysisWithDistributionImpl.java
@@ -1,4 +1,8 @@
 package org.ohdsi.cohortcharacterization.design;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.ohdsi.cohortcharacterization.utils.FeatureAnalysisDeserializer;
+
+@JsonDeserialize(using = FeatureAnalysisDeserializer.None.class)
 public class FeatureAnalysisWithDistributionImpl extends FeatureAnalysisWithCriteriaImpl<DistributionFeatureImpl> {
 }
---
+++ src/test/java/org/ohdsi/cohortcharacterization/CohortCharacterizationTest.java
@@ -0,0 +1,34 @@
+package org.ohdsi.cohortcharacterization;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.text.IsEmptyString.emptyString;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.ohdsi.analysis.Utils;
+import org.ohdsi.analysis.cohortcharacterization.design.CohortCharacterization;
+import org.ohdsi.circe.helper.ResourceHelper;
+import org.ohdsi.cohortcharacterization.design.CohortCharacterizationImpl;
+
+public class CohortCharacterizationTest {
+
+	private static final String CC = ResourceHelper.GetResourceAsString("/cohortcharacterization/CohortCharacterization.json");
+
+	@Test
+	public void testCohortCharacterization() {
+
+		CohortCharacterization cc = Utils.deserialize(CC, CohortCharacterizationImpl.class);
+		assertThat(cc, is(notNullValue()));
+	}
+
+	@Test
+	public void testBuildQuery() {
+
+		CCQueryBuilder builder = new CCQueryBuilder(CC, "cohort", "123456", "public", "results", "public", "results", 148);
+		String sql = builder.build();
+		assertThat(sql, is(notNullValue()));
+		assertThat(sql, not(emptyString()));
+	}
+}
